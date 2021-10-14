160d9e59e34309a4fbffe37bbd1d719915517d90
Tejbirsingh7878 <78433002+Tejbirsingh7878@users.noreply.github.com>
2021-10-14 22:00:07 +0530

    Update FactorialProgramUsingRecursion.java
    hello added a simple program of calculating NcR value. actually im learning how to make changes and to create pull  Request.
    is it a right way to create Pull Request? kindly accept my pull request. and sorry if i did something wrong..

--- JAVA/FactorialProgramUsingRecursion.java
+++ JAVA/FactorialProgramUsingRecursion.java
@@ -20,4 +20,40 @@ public class FactorialProgramUsingRecursion {
         factorial = Factorial(n);
         System.out.println("Factorial of "+ n + " is " + factorial);
     }
+    /*
+    enter n value 
+    10
+    enter r value 
+    8
+    
+    45
+    */
+    
+    public class NcR {
+
+	public static int factorial(int n) {
+		int ans=1;
+		for(int i=1;i<=n;i++) {
+			ans=ans*i;
+	}
+	return ans;
+	}
+
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter n value ");
+			int n=s.nextInt();
+		System.out.println("enter r value ");
+			int r=s.nextInt();
+		
+    	int num= factorial(n);
+		int deno1= factorial(r);
+		int deno2= factorial(n-r);
+		int ans=num/(deno1*deno2);
+		System.out.println(ans);	
+		}
+
+	}
+    
 }
